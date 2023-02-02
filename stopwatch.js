c6432829f3a79d11781e6e30f8d10bc7bdeaed03
flag3 <hts.a.y.225@gmail.com>
2023-02-02 13:02:28 +0900

    stopwatch lv2

--- stopwatch.js
+++ stopwatch.js
@@ -2,20 +2,35 @@ let startTime = 0
 let endTime = 0
 let time = 0
 let stat = "stop"
+let intervalID = null
+
+function startTimer () {
+  startTime = Date.now()
+  const stringElement = document.querySelector('#start')
+  stringElement.innerText = 'ストップ'
+}
+
+function stopTimer () {
+  const stringElement = document.querySelector('#start')
+  stringElement.innerText = 'スタート'
+}
+
+function rewritingTime () {
+  endTime = Date.now()
+  time = (endTime - startTime) / 1000
+  const timeElement = document.querySelector('#time')
+  timeElement.innerText = '時間: ' + time + '秒'
+}
 
 const timer = () => {
-  if (stat == "stop") {
-    startTime = Date.now()
-    const stringElement = document.querySelector('#start')
-    stringElement.innerText = 'ストップ'
+  if (stat === "stop") {
+    intervalID = setInterval(rewritingTime, 1)
+    startTimer()
     stat = "start"
   } else if (stat === "start") {
-    endTime = Date.now()
-    time = (endTime - startTime) / 1000
-    const timeElement = document.querySelector('#time')
-    timeElement.innerText = '時間: ' + time + '秒'
-    const stringElement = document.querySelector('#start')
-    stringElement.innerText = 'スタート'
+    clearInterval(intervalID)
+    intervalID = null
+    stopTimer()
     stat = "stop"
   } 
 }
