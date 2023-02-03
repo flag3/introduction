0629fba4da7015fcd6216e6765e577af73bf06bc
flag3 <hts.a.y.225@gmail.com>
2023-02-03 23:35:03 +0900

    fix lapTimer

--- stopwatch.js
+++ stopwatch.js
@@ -1,12 +1,14 @@
 let startTime = 0
 let currentTime = 0
 let temporaryTime = 0
+let lapTime = 0
+let lapStartTime = 0
 let timeStatus = "stop"
 let intervalID = null
 let lapCounter = 0
 let message = ""
 
-function startTimer () {
+function startTimer() {
   intervalID = setInterval(rewritingTime, 1)
   const startElement = document.querySelector('#start')
   startElement.innerText = 'ストップ'
@@ -16,7 +18,7 @@ function startTimer () {
   timeStatus = "start"
 }
 
-function stopTimer () {
+function stopTimer() {
   clearInterval(intervalID)
   intervalID = null
   const startElement = document.querySelector('#start')
@@ -30,25 +32,28 @@ function stopTimer () {
 function resetTimer() {
   temporaryTime = 0
   const timeElement = document.querySelector('#time')
-  timeElement.innerText = convertToTime(temporaryTime)
+  timeElement.innerText = convertToString(temporaryTime)
   message = ''
   document.getElementById("laptime").innerHTML = message;
   lapCounter = 0
+  lapStartTime = 0
 }
 
 function lapTimer() {
   lapCounter++
-  message += 'ラップ' + lapCounter + '&emsp;' + convertToTime(currentTime) + '<br>'
-  document.getElementById("laptime").innerHTML = message;
+  lapTime = currentTime - lapStartTime
+  message += '<br>ラップ' + lapCounter + '&emsp;' + convertToString(lapTime)
+  document.getElementById("laptime").innerHTML = message
+  lapStartTime = currentTime
 }
 
-function rewritingTime () {
+function rewritingTime() {
   currentTime = Date.now() + temporaryTime - startTime
   const timeElement = document.querySelector('#time')
-  timeElement.innerText = convertToTime(currentTime)
+  timeElement.innerText = convertToString(currentTime)
 }
 
-function convertToTime(time) {
+function convertToString(time) {
   const h = String(Math.floor(time / (60 * 60 * 1000))).padStart(2, '0')
   const m = String(Math.floor(time / (60 * 1000)) % 60).padStart(2, '0')
   const s = String(Math.floor(time / 1000) % 60).padStart(2, '0')
